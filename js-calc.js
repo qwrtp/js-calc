9d0b0f0d4d06336fb1d0ed249a774362a6f233ab
Me <me@my-PDP11.local>
2020-04-27 22:36:48 +0100

    Integer division, tabindex and css

--- js-calc.js
+++ js-calc.js
@@ -1,5 +1,12 @@
-function calc() {
+document.addEventListener("DOMContentLoaded", function() {
+  let button = document.querySelector("#calculate");
+  button.addEventListener("submit", function () {
+    calc(event);
+  });
+});
 
+function calc(event) {
+  event.preventDefault();
   a = parseInt(document.querySelector("#value1").value);
   b = parseInt(document.querySelector("#value2").value);
   var result;
@@ -18,8 +25,15 @@ function calc() {
   if (op == "div") {
     result = a / b;
   }
+  if (op == "divint") {
+    result = parseInt(a / b);
+  }
+  if (op == "mod") {
+    result = a % b;
+  }
 
   // Display result
   document.querySelector("#result").innerHTML = result;
+  console.log("electron moving handiwork by Bernard Cribbins");
 
 }
